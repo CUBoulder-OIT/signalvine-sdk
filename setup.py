40d66f3e8b76177d1a52b0415e2009bc9504cef2
Steve Taylor <staylorx@gmail.com>
2021-03-08 13:39:45 -0700

    Simplify build to 3.8 and try again for prod pypi

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if isfile(requirement_path):
 # This call to setup() does all the work
 setup(
     name="signalvine-sdk",
-    version="0.4.5",
+    version="0.4.6",
     description="SignalVine API SDK",
     long_description=README,
     long_description_content_type="text/markdown",
